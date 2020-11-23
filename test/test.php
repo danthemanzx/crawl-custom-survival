e7a01840618aba45581063dab511008e7f213e4b
danielssouthworth <60378084+danielssouthworth@users.noreply.github.com>
2020-11-23 15:06:50 -0600

    test

---
+++ test/test.php
@@ -0,0 +1,40 @@
+<?php
+    // Edit this ->
+    define( 'MQ_SERVER_ADDR', 'localhost' );
+    define( 'MQ_SERVER_PORT', 25565 );
+    define( 'MQ_TIMEOUT', 1 );
+    // Edit this <-
+
+    // Display everything in browser, because some people can't look in logs for errors
+    Error_Reporting( E_ALL | E_STRICT );
+    Ini_Set( 'display_errors', false );
+
+    require __DIR__ . '/MinecraftQuery.class.php';
+
+    $Timer = MicroTime( true );
+
+    $ServerQuery = new MinecraftQuery( );
+
+    try
+    {
+        $ServerQuery->Connect( MQ_SERVER_ADDR, MQ_SERVER_PORT, MQ_TIMEOUT );
+    }
+    catch( MinecraftQueryException $e )
+    {
+        $Exception = $e;
+    }
+
+    $Timer = Number_Format( MicroTime( true ) - $Timer, 4, '.', '' );
+
+    $serverInfo = $ServerQuery->GetInfo();
+?>
+Players Online: <?php echo $serverInfo['Players'] . "/" . $serverInfo['MaxPlayers']; ?>
+<?php
+    if ( ( $Players = $ServerQuery->GetPlayers( ) ) !== false ) {
+        foreach($Players as $Player){
+                echo "<img width='64' height='64' src='http://cravatar.eu/head/".$Player."/64.png' />";
+            }
+    }  else {
+        echo "Nobodys online!";
+        }
+    ?>
