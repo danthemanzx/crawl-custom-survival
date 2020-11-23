24942a6f08ae3c77f9717b36dc69d8f200ddfb30
danielssouthworth <60378084+danielssouthworth@users.noreply.github.com>
2020-11-23 04:18:46 -0600

    Test

--- test/onlineplayers-lib/src/main/java/de/bmarwell/minecraft/domain/MinecraftPlayerStatus.java
+++
@@ -1,9 +0,0 @@
-package de.bmarwell.minecraft.domain;
-
-
-public class MinecraftPlayerStatus {
-
-    private int hearts;
-
-    private int armor;
-}
--- test/onlineplayers-lib/src/main/java/de/bmarwell/minecraft/domain/MinecraftPosition.java
+++
@@ -1,9 +0,0 @@
-package de.bmarwell.minecraft.domain;
-
-public class MinecraftPosition {
-    private long x;
-
-    private long y;
-
-    private long z;
-}
