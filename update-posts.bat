2fd13917eddd3d0ab6bd519a50bf02f74b3720c6
danielssouthworth <60378084+danielssouthworth@users.noreply.github.com>
2020-11-25 02:56:45 -0600

    This is a test

--- update-posts.bat
+++ update-posts.bat
@@ -3,5 +3,7 @@ title Updating Post Files via Github Repository Wiki
 echo "Updating files..."
 echo "Warning: This will clear all existing posts. Press any key to continue..."
 pause
-start "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "rm -rf _posts && mkdir _posts && cd _posts && git clone https://github.com/danthemanzx/crawl-custom-survival.wiki.git . && cd .. && git mv -f _posts/Home.md pages && git mv -f _posts/_Sidebar.md pages ; bash"
+start "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "rm -rf _posts && mkdir _posts && cd _posts && git clone https://github.com/danthemanzx/crawl-custom-survival.wiki.git . && cd .. && git mv -f _posts/Home.md pages && git mv -f _posts/_Sidebar.md pages && exit; bash"
+echo "Done!"
+pause
 exit
