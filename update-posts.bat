4fcf87e051d41e2a3705f2c2ae17d267848fdadb
danielssouthworth <60378084+danielssouthworth@users.noreply.github.com>
2021-01-18 18:54:12 -0600

    Updates

--- update-posts.bat
+++ update-posts.bat
@@ -4,6 +4,6 @@ echo "Updating files..."
 echo "Warning: This will clear all existing posts. Press any key to continue..."
 start "" "%PROGRAMFILES%\Git\bin\sh.exe" -c "rm -rf _posts && mkdir _posts && cd _posts && git clone https://github.com/danthemanzx/crawl-custom-survival.wiki.git . && exit; bash"
 move /Y _posts\Home.md pages\Home.md
-move /Y _posts\_Sidebar.md pages\_Sidebar.md
+move /Y _posts\_Sidebar.md _layouts\_Sidebar.md
 echo "Done!"
 exit
