e3534c65ec9fe7ce87473c3780689d80483a897b
danielssouthworth <60378084+danielssouthworth@users.noreply.github.com>
2021-01-25 20:06:44 -0600

    Update donate.js

--- assets/donate.js
+++ assets/donate.js
@@ -1,1 +1,1 @@
-window.location.replace("https://www.paypal.com/donate?business=F64ENR75ND792&item_name=Used+to+fund+Mine-Crawl+Minecraft+server&currency_code=USD");
+window.location.replace("https://www.paypal.com/paypalme/minecrawl");
